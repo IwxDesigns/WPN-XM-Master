d1e222497bd66f1a832fee116daa1a15d07b2b23
Jerrell Rose <jerrellrose@msn.com>
2019-01-09 19:03:06 -0600

    A complete copy of the WXN-XM project

---
+++ git update.bat
@@ -0,0 +1,95 @@
+
+@echo off
+echo.
+echo WPN-XM-maste Git Update
+echo updating api-client, benchmark-tools, docs, marketing, php-software
+echo qt-mini-deploy, registry, server-control-panel, software, updater
+echo vcredist, webinterface, website-wpn-xm.org, WPN-XM
+
+cd api-client
+echo api-client Git Update 
+git pull
+echo.
+cd ..
+
+
+cd benchmark-tools
+echo benchmark-tools Git Update 
+git pull
+echo.
+cd ..
+
+cd docs
+echo docs Git Update 
+git pull
+echo.
+cd ..
+
+cd marketing
+echo marketing Git Update 
+git pull
+echo.
+cd ..
+
+cd php-software
+echo php-software Git Update 
+git pull
+echo.
+cd ..
+
+cd qt-mini-deploy
+echo qt-mini-deploy Git Update 
+git pull
+echo.
+cd ..
+
+cd registry
+echo registry Git Update 
+git pull
+echo.
+cd ..
+
+cd server-control-panel
+echo server-control-panel Git Update 
+git pull
+echo.
+cd ..
+
+cd software
+echo software Git Update 
+git pull
+echo.
+cd ..
+
+cd updater
+echo updater Git Update 
+git pull
+echo.
+cd ..
+
+cd vcredist
+echo vcredist Git Update 
+git pull
+echo.
+cd ..
+
+cd webinterface
+echo webinterface Git Update 
+git pull
+echo.
+cd ..
+
+cd website-wpn-xm.org
+echo website-wpn-xm.org Git Update 
+git pull
+echo.
+cd ..
+
+cd WPN-XM
+echo WPN-XM Git Update 
+git pull
+echo.
+cd ..
+cd ..
+
+pause
